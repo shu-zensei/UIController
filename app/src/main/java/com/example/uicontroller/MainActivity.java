6f3ff5c37a2f21bd76d46db07ca88f0a77658a1c
shu-zensei <shanqing3131@gmail.com>
2022-03-14 14:33:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/uicontroller/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.uicontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button1;
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView1);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("左へ");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("右へ");
+            }
+        });
+    }
+}
